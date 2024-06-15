adeb74dc17473a3105f1ec2d122ce486ee00a5f1
coder-noman <abdhullaalnoman25@gmail.com>
2024-06-15 13:17:41 +0600

    final Commit

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chat-app-personal-cq9d.onrender.com", {
         query: {
           userId: authUser._id,
         },
